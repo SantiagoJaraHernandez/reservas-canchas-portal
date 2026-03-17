4e4851a4e4dbf0d89142774b70bfe66a03da48ef
Sneyder14 <bediaz2021a@corhuila.edu.co>
2026-03-17 14:09:33 -0500

    refactor: color change for reservationPage

--- Frontend-Reservas-Canchas/src/pages/ReservationPage.jsx
+++ Frontend-Reservas-Canchas/src/pages/ReservationPage.jsx
@@ -5,7 +5,7 @@ export default function ReservationPage() {
         <div className="w-full max-w-xl glass-panel rounded-3xl flex flex-col shadow-2xl container__formulario gap-5">
             <div className="text-center mb-10">
                 <h1 className="text-4xl font-bold text-white mb-2">Reserva tu Cancha</h1>
-                <p className="text-slate-300">
+                <p className="text-slate-500">
                     Completa los detalles para asegurar tu lugar en el campo.
                 </p>
             </div>
