97f94de7050cb3ca0b6d0265d4b29ec39d2a924e
Sneyder14 <bediaz2021a@corhuila.edu.co>
2026-04-06 20:38:33 -0500

    feat(auth): update login/register components, add stadium assets, and remove unused react.svg

--- Frontend-Reservas-Canchas/src/components/FormAuth/FormAuth.jsx
+++ Frontend-Reservas-Canchas/src/components/FormAuth/FormAuth.jsx
@@ -4,14 +4,15 @@ import Titulo from "../texts/Title";
 import SubTitulo from "../texts/SubTitle";
 import BtnAggReserva from "../Botones/ButtonReservation";
 
-function FormAuth({ title, subTtitle, fields, terms, icono }) {
+
+function FormAuth({ title, subTtitle, fields, terms, className = "", textButton }) {
     return (
-        <form action="" className="flex flex-col bg-white max-w-100 w-full px-8 py-4 rounded-2xl shadow-sm shadow-primary/80 gap-10 relative overflow-hidden">
+        <form action="" className={`flex flex-col bg-white max-w-100 w-full px-8 py-4 rounded-2xl shadow-sm shadow-primary/80 gap-10 relative overflow-hidden ${className}`}>
             <div className="clip-top bg-primary h-100 w-full absolute inset-0 "></div>
             <div className="text-center flex flex-col z-10">
-                <Icon name={icono} className="font-bold text-white" />
-                <Titulo titulo={title} className="text-2xl font-bold" />
-                <SubTitulo subTitle={subTtitle} className="text-sm text-slate-600" />
+                <Icon name={"sports_soccer"} className="font-bold text-white" />
+                <Titulo titulo={title} className="text-3xl font-bold text-white" />
+                <SubTitulo subTitle={subTtitle} className="text-sm text-slate-200" />
             </div>
             <div className="flex flex-col gap-3">
                 {fields.map((input) => (
@@ -21,17 +22,19 @@ function FormAuth({ title, subTtitle, fields, terms, icono }) {
                         placeholder={input.placeholder}
                         icono={input.icono}
                         type={input.type}
+
                     />
                 ))}
                 <div className="flex text-[12px] py-2 gap-2">
                     <input type="checkbox" name="" id="" className="cursor-pointer" />
-                    <p className="">{terms} </p>
+                    <p className="text-slate-50">{terms} Acepto los <span className="text-primary font-bold">Términos de Servicio </span>y la <span className="text-primary font-bold">Política de Privacidad </span> de SoccerField Manager.</p>
                 </div>
             </div>
             <div className="flex font-bold">
                 <BtnAggReserva className="flex flex-1 items-center justify-center"
                     icono={"arrow_forward"}
-                    texto={"Registrate"} />
+                    texto={textButton}
+                />
             </div>
         </form>
     )
