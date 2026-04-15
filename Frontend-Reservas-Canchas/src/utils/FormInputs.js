92439669a9c08ad7097f945cb60cfbd59a996e8c
Santiago Jara Hernandez <ryunox999@gmail.com>
2026-04-15 13:51:23 -0500

    fix(reservas): remove user id input from reservation forms

--- Frontend-Reservas-Canchas/src/utils/FormInputs.js
+++ Frontend-Reservas-Canchas/src/utils/FormInputs.js
@@ -1,7 +1,31 @@
 export const inputs = [
-    [{ name: "idUsuario", label: "User ID", type: "number", placeholder: "Ej: 2", icono: "person", },
-    { name: "idCancha", label: "Cancha ID", type: "number", placeholder: "Ej: 2", icono: "stadium" }],
-    [{ name: "fecha", label: "Fehca", type: "date", placeholder: "" },
-    { name: "horaInicio", label: "Hora Inicio", type: "time", placeholder: "", },
-    { name: "horaFin", label: "Hora Fin", type: "time", placeholder: "" },]
-]
+  [
+    {
+      name: "idCancha",
+      label: "Cancha ID",
+      type: "number",
+      placeholder: "Ej: 1",
+      icono: "stadium",
+    },
+  ],
+  [
+    {
+      name: "fecha",
+      label: "Fecha",
+      type: "date",
+      placeholder: "",
+    },
+    {
+      name: "horaInicio",
+      label: "Hora Inicio",
+      type: "time",
+      placeholder: "",
+    },
+    {
+      name: "horaFin",
+      label: "Hora Fin",
+      type: "time",
+      placeholder: "",
+    },
+  ],
+];
